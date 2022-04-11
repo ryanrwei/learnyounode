6721c6af05dc3652f2bd5f9c624b7c43d8ade3d1
ryanrwei <r.a1996315@gmail.com>
2022-04-11 20:29:54 +0800

    add index.js file

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("Hello World")
+const func = () => {
+    console.log("Hi")
+}
+func()
+const num = 7;
+const obj = { num }
+console.log(obj)
+const result = JSON.parse('[1, 5, "false"]');
+console.log(result[0])
+const jsonStr = JSON.stringify([1, 'false', false]);
+console.log(typeof jsonStr)
